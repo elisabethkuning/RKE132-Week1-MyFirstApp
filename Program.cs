a0ef33ce383dcc854c62846a1a91c1bd83ffccb3
elisabethkuning <ekuning@tktk.ee>
2023-02-06 20:21:53 +0200

    create new app

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+//console - objekt
+//rakendus küsib kasutajalt sisestada tema nimi
+//rakendus tervitab kasutajat nime pidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+
+string userName = Console.ReadLine();//Input
+
+Console.WriteLine ("Hello"+","+userName+"!");//Output
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");//Output
